45346209b4d411095e3def1ce524f38c6d906b21
Garland Xie <garlandxie@gmail.com>
2021-09-14 21:44:37 -0400

    Made a list of file paths

--- src/01-data_cleaning.R
+++ src/01-data_cleaning.R
@@ -1,2 +1,11 @@
 # libraries -----
-library(here)
+library(here)   # for creating relative file-paths
+library(tidyr)
+
+# import ----
+
+myfiles <- list.files(
+  path = here("data/original"),
+  pattern = "*.csv", 
+  full.names = TRUE
+)
